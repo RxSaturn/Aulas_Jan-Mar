388bec7ebf70023216682c8a83efb38b524938b6
Mauro Henrique Silva <silva.maurohenrique@gmail.com>
2026-01-06 23:34:43 +0000

    feat: Projeto - Aula 06-01

---
+++ Aula_06-01/projeto01/src/Components/Header.jsx
@@ -0,0 +1,14 @@
+const Cabecalho = () => {
+  return (
+    <header>
+      <h1>Nome da empresa</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nemo
+        animi soluta maxime iure minima placeat officia magnam, fugit, atque
+        deserunt sint vel quam facilis laboriosam ex ipsam dolorem optio.
+      </p>
+    </header>
+  );
+};
+
+export default Cabecalho;
