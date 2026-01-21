6ebd81d68c2712d91114f10e6727ce5c2720d294
Mauro Henrique Silva <silva.maurohenrique@gmail.com>
2026-01-21 19:21:55 +0000

    feat: Estilizacao do button

--- Aula_20-01/projeto05/src/components/Button.jsx
+++ Aula_20-01/projeto05/src/components/Button.jsx
@@ -1,6 +1,8 @@
+import "./Button.css";
 const Button = ({ label, nome, tipo, onClick, disabled }) => {
   return (
     <button
+      className="btn"
       id={nome}
       name={nome}
       type={tipo}
